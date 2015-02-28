7d32e12de47dabb522cb119ab71d2547477181e5
endotakashi1992 <endotakashi1992@gmail.com>
2015-03-01 07:36:06 +0900

    redis

---
+++ api/controllers/PipeController.js
@@ -0,0 +1,11 @@
+/**
+ * PipeController
+ *
+ * @description :: Server-side logic for managing pipes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
