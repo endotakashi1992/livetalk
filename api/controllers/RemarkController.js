5ce329d28eacb6a4052b02103c92a6853c4c03cc
endotakashi1992 <endotakashi1992@gmail.com>
2015-03-04 17:19:07 +0900

    user and remark with one to one

---
+++ api/controllers/RemarkController.js
@@ -0,0 +1,11 @@
+/**
+ * RemarkController
+ *
+ * @description :: Server-side logic for managing remarks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
